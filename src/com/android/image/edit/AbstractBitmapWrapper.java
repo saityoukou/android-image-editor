ced8bef811dbad6fec215aa84e504cf0b6c0a39c
sai.tyoukou <sai_tyouko@amesta.co.jp>
2015-04-28 12:41:29 +0900

    m2

--- src/com/android/image/edit/AbstractBitmapWrapper.java
+++ src/com/android/image/edit/AbstractBitmapWrapper.java
@@ -9,7 +9,7 @@ import android.graphics.PorterDuffXfermode;
 public abstract class AbstractBitmapWrapper implements BitmapWrapper {
 	
 	protected Paint pathPaint;
-    int m = 1;
+    int m = 12;
 	
 	public AbstractBitmapWrapper(boolean originalBitmap) {
 		pathPaint = createPaint(originalBitmap);
